3b2281b6461fe8b9d34595e7f5037a5735fe42dc
Satish Goda <satishgoda@gmail.com>
2014-10-04 02:48:53 +0800

    Window  (slot) <--> Widget <--> Button (signal)

--- pyside/masterapp/masterapp.py
+++ pyside/masterapp/masterapp.py
@@ -1,3 +1,4 @@
+import PySide
 from PySide import QtGui, QtCore
 
 if __name__ == "__main__":
@@ -6,9 +7,14 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
 
     window = QtGui.QMainWindow()
-
     window.setWindowTitle("Hello PySide")
     window.setFixedSize(500, 500)
+
+    widget = QtGui.QWidget()
+    button = QtGui.QPushButton("Close Window", parent=widget)
+    button.clicked.connect(window.close)
+
+    window.setCentralWidget(widget)
 
     window.show()
 
