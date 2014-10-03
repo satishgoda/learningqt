6005aeabbca880833f3df54fc9fee36975dc5323
Satish Goda <satishgoda@gmail.com>
2014-10-04 03:09:13 +0800

    Subclassing QMainWindow and override close method

--- pyside/masterapp/masterapp.py
+++ pyside/masterapp/masterapp.py
@@ -1,20 +1,31 @@
 import PySide
 from PySide import QtGui, QtCore
 
+
+class MyMainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MyMainWindow, self).__init__()
+        self._initUI()
+
+    def _initUI(self):
+        self._widget = QtGui.QWidget()
+        self.setCentralWidget(self._widget)
+        self._button = QtGui.QPushButton("Close Window", parent=self._widget)
+        self._button.clicked.connect(self.close)
+
+    def close(self):
+        print("Closing")
+        super(MyMainWindow, self).close()
+
+
 if __name__ == "__main__":
     import sys
 
     app = QtGui.QApplication(sys.argv)
 
-    window = QtGui.QMainWindow()
+    window = MyMainWindow()
     window.setWindowTitle("Hello PySide")
     window.setFixedSize(500, 500)
-
-    widget = QtGui.QWidget()
-    button = QtGui.QPushButton("Close Window", parent=widget)
-    button.clicked.connect(window.close)
-
-    window.setCentralWidget(widget)
 
     window.show()
 
