b26b258ecd65e3b3c25d2a975fc179038f52ba19
Satish Goda <satishgoda@gmail.com>
2014-10-20 23:14:04 +0800

    Added vertical layout containing push buttons and a spacer.
    When the layout object is instantiated, the widget is passed as parent. Push buttons and spacer are constructed with the widget as parent.
    
    The push buttons and spacer widgets are then added to the layout.

--- pyside/designer/experiments/default.py
+++ pyside/designer/experiments/default.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'default.ui'
 #
-# Created: Mon Oct 20 22:47:29 2014
+# Created: Mon Oct 20 23:05:48 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,6 +17,27 @@ class Ui_Form(object):
         Form.setMaximumSize(QtCore.QSize(400, 400))
         Form.setStyleSheet("background-color: rgb(153, 153, 153);\n"
 "")
+        self.verticalLayout = QtGui.QVBoxLayout(Form)
+        self.verticalLayout.setSpacing(3)
+        self.verticalLayout.setContentsMargins(3, 3, 3, 3)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton_load = QtGui.QPushButton(Form)
+        self.pushButton_load.setObjectName("pushButton_load")
+        self.verticalLayout.addWidget(self.pushButton_load)
+        self.pushButton_run = QtGui.QPushButton(Form)
+        self.pushButton_run.setObjectName("pushButton_run")
+        self.verticalLayout.addWidget(self.pushButton_run)
+        self.pushButton_view = QtGui.QPushButton(Form)
+        self.pushButton_view.setObjectName("pushButton_view")
+        self.verticalLayout.addWidget(self.pushButton_view)
+        self.pushButton_unload = QtGui.QPushButton(Form)
+        self.pushButton_unload.setObjectName("pushButton_unload")
+        self.verticalLayout.addWidget(self.pushButton_unload)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.pushButton_close = QtGui.QPushButton(Form)
+        self.pushButton_close.setObjectName("pushButton_close")
+        self.verticalLayout.addWidget(self.pushButton_close)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -25,6 +46,11 @@ class Ui_Form(object):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
         Form.setToolTip(QtGui.QApplication.translate("Form", "I cannot be resized at all. Sorry!", None, QtGui.QApplication.UnicodeUTF8))
         Form.setWhatsThis(QtGui.QApplication.translate("Form", "I am a widget that cannot be resized", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_load.setText(QtGui.QApplication.translate("Form", "Load", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_run.setText(QtGui.QApplication.translate("Form", "Run", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_view.setText(QtGui.QApplication.translate("Form", "View", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_unload.setText(QtGui.QApplication.translate("Form", "Unload", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_close.setText(QtGui.QApplication.translate("Form", "Close", None, QtGui.QApplication.UnicodeUTF8))
 
 
 if __name__ == "__main__":
