4e50fc8742183badf20f5292b3ebe86e1e90ce4f
Satish Goda <satishgoda@gmail.com>
2014-10-20 22:37:28 +0800

    Underdtanding Qt Designer and pyside-uic
    pyside-uic -h
    
    pyside-uic -p -d default.ui
    
    pyside-uic -x -d -o default.py default.ui

---
+++ pyside/designer/experiments/default.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'default.ui'
+#
+# Created: Mon Oct 20 22:31:22 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(591, 411)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
