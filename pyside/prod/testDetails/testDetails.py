dccbf820c255198f699cd166d64f66a882616359
Satish Goda <satishgoda@gmail.com>
2014-10-22 23:00:18 +0800

    Changed import style and test names with layout

--- pyside/prod/testDetails/testDetails.py
+++ pyside/prod/testDetails/testDetails.py
@@ -1,15 +1,17 @@
 
 from PySide import QtCore, QtGui
 
-from testDetailsDialog import Ui_Dialog
+import testDetailsDialog
 
 
-class TestDetailsDialog(QtGui.QDialog, Ui_Dialog):
+class TestDetailsDialog(QtGui.QDialog, testDetailsDialog.Ui_Dialog):
     def __init__(self, tester):
         super(TestDetailsDialog, self).__init__()
         self.setupUi(self)
         self._tester = tester
         self.updateUi()
+        self.setWindowTitle("Validation Test Details")
+        self.detailsLabel.setText(self._tester.__class__.__name__)
 
     def layout(self, *args, **kwargs):
         return self.detailsPlaceholder.layout()
@@ -23,8 +25,8 @@ class BaseTest(object):
         pass
 
 
-class ZeroAreaTest(BaseTest):
-    description = "Zero area faces were found for the following"
+class InsanityTest(BaseTest):
+    description = "Insane subjects were found as following"
 
     def __init__(self):
         super(BaseTest, self).__init__()
@@ -32,17 +34,26 @@ class ZeroAreaTest(BaseTest):
     def drawUi(self, parent):
         layout = parent.layout()
         parent.detailsDescriptionLabel.setText(self.description)
-        for i in range(0, 50):
-            pb = QtGui.QPushButton(parent)
-            layout.addWidget(pb)
+        for index in range(0, 25):
+            hl = QtGui.QHBoxLayout()
+            layout.addLayout(hl)
+            lbl = QtGui.QLabel(parent)
+            lbl.setText(str(index+1))
+            pb1 = QtGui.QPushButton(parent)
+            pb1.setText("Select")
+            pb2 = QtGui.QPushButton(parent)
+            pb2.setText("Fix")
+            hl.addWidget(lbl)
+            hl.addWidget(pb1)
+            hl.addWidget(pb2)
 
 
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
 
-    zat = ZeroAreaTest()
-    tdd = TestDetailsDialog(zat)
+    it = InsanityTest()
+    tdd = TestDetailsDialog(it)
     tdd.show()
 
     sys.exit(app.exec_())
