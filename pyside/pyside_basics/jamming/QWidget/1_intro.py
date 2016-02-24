47dd4e10f39487edd1dd0f1f45a953622e2a7ae5
Satish Goda <satishgoda@live.com>
2016-02-24 23:14:02 +0800

    QTreeWidget intro

--- pyside/pyside_basics/jamming/QWidget/1_intro.py
+++ pyside/pyside_basics/jamming/QWidget/1_intro.py
@@ -29,3 +29,39 @@ mainLayout.update()
 mainLayout.setAlignment(QtCore.Qt.AlignTop)
 
 mainLayout.update()
+
+################
+
+qtw = QtGui.QTreeWidget()
+mainLayout.addWidget(qtw)
+
+################
+
+qtw.setColumnCount(2)
+
+################
+
+qtwhi = QtGui.QTreeWidgetItem()
+qtwhi.setText(0, "Namespace")
+qtwhi.setText(1, "Representation")
+qtw.setHeaderItem(qtwhi)
+
+################
+
+qtw.setColumnWidth(0, 150)
+
+################
+
+for rig in pm.ls(regex="*:*_rig"):
+    qtwtli = QtGui.QTreeWidgetItem()
+    qtwtli.setText(0, rig.namespace())
+    qtwtli.setText(1, rig.attr('representations').get())
+    qtw.addTopLevelItem(qtwtli)
+
+################
+
+
+################
+
+
+################
