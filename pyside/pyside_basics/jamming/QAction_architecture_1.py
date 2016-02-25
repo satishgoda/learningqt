0c12b9e6dc7a0d5fda3170dd0c38ce4be2f882b0
Satish Goda <satishgoda@live.com>
2016-02-25 22:17:34 +0800

    QWidget, QAction and the Observer Pattern

--- pyside/pyside_basics/jamming/QAction_architecture_1.py
+++ pyside/pyside_basics/jamming/QAction_architecture_1.py
@@ -1,11 +1,57 @@
+#######
 
 from PySide.QtGui import QWidget, QPushButton, QAction
 from PySide.QtGui import QHBoxLayout
+
+#######
 
 
 class Context(object):
     def __init__(self):
         self.state = False
+    
+    @property
+    def state(self):
+        return self._state
+    
+    @state.setter
+    def state(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("Invalid state")
+        self._state = value
+
+#######
+
+
+ctx = Context()
+
+#######
+
+
+print Context.__dict__['state']
+
+#######
+
+
+print ctx.__dict__
+
+#######
+
+try:
+    ctx.state = 1
+except ValueError as e:
+    print e
+    raise e
+
+#######
+
+ctx.state = True
+
+#######
+
+print ctx.__dict__
+
+#######
 
 
 class SelectionChanged(QAction):
@@ -13,21 +59,25 @@ class SelectionChanged(QAction):
         super(SelectionChanged, self).__init__(widget)
         self.triggered.connect(self.timeToInform)
         self.context = Context()
-    
+        
+    def inform(self, informee):
+        informee.addAction(self)
+
     @property
     def informees(self):
         return self.associatedWidgets()
-    
-    def inform(self, informee):
-        informee.addAction(self)
 
     def updateContext(self, value):
         self.context.state = value
         self.trigger()
-    
+
     def timeToInform(self):
         for informee in self.informees:
             informee.refresh(self.context)
+
+    
+
+#######
 
 
 class Tool(QPushButton):
@@ -36,6 +86,8 @@ class Tool(QPushButton):
     
     def refresh(self, context):
         self.setEnabled(context.state)
+
+#######
 
 
 class ToolBar(QWidget):
@@ -60,12 +112,60 @@ class ToolBar(QWidget):
         layout = self.layout()
         layout.insertWidget(layout.count()-1, tool)
 
+#######
+
 
 toolbar = ToolBar()
+
+#######
+
+
+toolbar.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
 toolbar.show()
 
+
+#######
+
+
 toolbar.addTool("Tool 1")
+
+#######
+
+
 toolbar.addTool("Tool 2")
 
-toolbar.onSelectionChanged.updateContext(False)
+#######
+
+
+try:
+    toolbar.onSelectionChanged.updateContext(1)
+except ValueError as e:
+    print e
+    raise
+
+"""
+>>> (executing lines 139 to 145 of "qaction.py")
+Invalid state
+Traceback (most recent call last):
+  File "C:\Users\looney\Documents\maya\scripts\learning\pyside\qaction.py", line 145, in <module>
+    raise e
+ValueError: Invalid state
+
+>>> (executing lines 139 to 145 of "qaction.py")
+Invalid state
+Traceback (most recent call last):
+  File "C:\Users\looney\Documents\maya\scripts\learning\pyside\qaction.py", line 142, in <module>
+    toolbar.onSelectionChanged.updateContext(1)
+  File "C:\Users\looney\Documents\maya\scripts\learning\pyside\qaction.py", line 71, in updateContext
+    self.context.state = value
+  File "C:\Users\looney\Documents\maya\scripts\learning\pyside\qaction.py", line 20, in state
+    raise ValueError("Invalid state")
+ValueError: Invalid state
+"""
+
+#######
+
 toolbar.onSelectionChanged.updateContext(True)
+
+#######
