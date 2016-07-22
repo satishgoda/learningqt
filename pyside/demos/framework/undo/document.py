117e07cec60926db636fb06b4b85d9e3c5a2256a
Satish Goda <satishgoda@live.com>
2016-07-23 07:50:58 +0800

    Update document.py

--- pyside/demos/framework/undo/document.py
+++ pyside/demos/framework/undo/document.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 from PySide.QtGui import QWidget, QPalette, QPainter
-from PySide.QtCore import Qt, QRect
+from PySide.QtCore import Qt, QRect, QSize, Signal, Slot
 
 
 ShapeType = namedtuple('ShapeType', 'Rectangle Circle Triangle')(*range(3))
@@ -12,6 +12,19 @@ class Shape(object):
         self._type = type
         self._color = color
         self._rect = rect
+        self._name = self.typeToString(type)
+
+    @property
+    def minSize(self):
+        return QSize(80, 50)
+    
+    @classmethod
+    def typeToString(cls, type):
+        return ShapeType._fields[type] 
+    
+    @classmethod
+    def stringToType(self, s, ok=0):
+        return ShapeType._asdict()[s]
 
     @property
     def type(self):
@@ -31,9 +44,14 @@ class Shape(object):
         
 
 class Document(QWidget):
+    currentShapeChanged = Signal(str)
+    
     def __init__(self, parent=None):
         super(Document, self).__init__(parent)
         self._shapeList = []
+        
+        self.mousePressIndex = -1
+        self.mousePressOffset = 0
         
         self.setAutoFillBackground(True)
         self.setBackgroundRole(QPalette.Base)
@@ -41,6 +59,33 @@ class Document(QWidget):
         pal = QPalette()
         pal.setColor(QPalette.HighlightedText, Qt.red)
         self.setPalette(pal)
+
+    def mouseMoveEvent(self, event):
+        event.accept()
+        
+        shape = self._shapeList[self.mousePressIndex]
+        rect  = shape.rect
+        
+        rect.moveTopLeft(event.pos() - self.mousePressOffset)
+        
+        size = rect.size().expandedTo(shape.minSize)
+        rect.setSize(size)
+        
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        event.accept()
+        self.mousePressIndex = -1
+
+    def mousePressEvent(self, event):
+        event.accept()
+        
+        for index, shape in enumerate(self._shapeList):
+            if shape.rect.contains(event.pos()):
+                self.mousePressIndex = index
+                self.mousePressOffset = event.pos() - shape.rect.topLeft()
+                print shape.name
+                break
 
     def paintEvent(self, event):
         paintRegion = event.region()
@@ -63,6 +108,10 @@ class Document(QWidget):
             elif shapeType == ShapeType.Circle:
                 print "circle"
                 painter.drawEllipse(rect)
+            
+            # paint the shape name
+            painter.setBrush(pal.text())
+            painter.drawText(rect, Qt.AlignCenter, shape.name)
 
 s1 = Shape(ShapeType.Rectangle, color=Qt.green, rect=QRect(0, 0, 100, 100))
 s2 = Shape(ShapeType.Circle, rect=QRect(200, 200, 100, 100))
