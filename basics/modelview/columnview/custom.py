50adf403c48889979cd1a417d6957cdb036f6c90
Satish Goda <satishgoda@live.com>
2016-09-21 23:23:26 +0800

    Update custom.py

--- basics/modelview/columnview/custom.py
+++ basics/modelview/columnview/custom.py
@@ -1,12 +1,12 @@
-class CustomColumnView(ColumnView):
+class CustomColumnView(QtGui.QColumnView):
     def __init__(self, *args, **kwargs):
         super(CustomColumnView, self).__init__(*args, **kwargs)
-        self.activated.connect(self.onActivated)
-    
-    def keyPressEvent(self, event):
-        super(CustomColumnView, self).keyPressEvent(event)
-        index = self.selectedIndexes()[0]
-        self.activated.emit(index)
 
-    def onActivated(self, index):
-        print index.data()
+    def selectionChanged(self, selected, deselected):
+        curindex = selected.indexes()[0]
+        print 'current: ', curindex.data()
+        try:
+            previndex = deselected.indexes()[0]
+            print 'previous: ',previndex.data()
+        except IndexError:
+            pass
