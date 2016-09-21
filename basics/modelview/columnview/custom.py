031b41e18ca1a2a94e304bfc117747d2fb1d3487
Satish Goda <satishgoda@live.com>
2016-09-22 00:14:08 +0800

    Update custom.py

--- basics/modelview/columnview/custom.py
+++ basics/modelview/columnview/custom.py
@@ -2,11 +2,21 @@ class CustomColumnView(QtGui.QColumnView):
     def __init__(self, *args, **kwargs):
         super(CustomColumnView, self).__init__(*args, **kwargs)
 
+    def setModel(self, *args, **kwargs):
+        super(CustomColumnView, self).setModel(*args, **kwargs)
+        width = self.columnWidths()[0]
+        self.setMinimumWidth(width*3)
+
     def selectionChanged(self, selected, deselected):
+        def path(index):
+            return "{0}/{1}".format(index.parent().data(), index.data())
+
         curindex = selected.indexes()[0]
-        print 'current: ', curindex.data()
+
         try:
             previndex = deselected.indexes()[0]
-            print 'previous: ',previndex.data()
+            navigation_path = ' -> '.join(map(path, (previndex, curindex)))
         except IndexError:
-            pass
+            navigation_path = path(curindex)
+        
+        self.setWindowTitle(navigation_path)
